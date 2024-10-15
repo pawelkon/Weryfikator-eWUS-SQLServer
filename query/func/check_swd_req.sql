fdc7ccaede8bbdb57b95ea57273c87545302b3f0
pawelkon <50082673+pawelkon@users.noreply.github.com>
2024-10-15 22:31:35 +0200

    code styling in check_swd_req file
    improving code styling

--- query/func/check_swd_req.sql
+++ query/func/check_swd_req.sql
@@ -2,10 +2,10 @@ CREATE FUNCTION checkSWDReq()
 RETURNS CHAR(2)
 AS
 BEGIN
-	RETURN (SELECT Value FROM OperatorConfig 
-			WHERE Param = 'id_OW' 
-			AND Value IN 
-				( SELECT * FROM STRING_SPLIT ( 
-					(SELECT Value FROM ServiceConfig 
-						WHERE Param = 'OW_SWD_required'), ',')))
+	RETURN (SELECT [Value] FROM OperatorConfig 
+			WHERE [Param] = 'id_OW' 
+			AND [Value] IN 
+				(SELECT * FROM STRING_SPLIT ( 
+					(SELECT [Value] FROM ServiceConfig 
+						WHERE [Param] = 'OW_SWD_required'), ',')))
 END
